7d1af2e7451fb300f6555eeb1a2b15e502852459
Jake Huey <125854542+jhueyy@users.noreply.github.com>
2025-09-30 15:34:40 -0700

    Added files from external libs and testing

--- data/cart.js
+++ data/cart.js
@@ -1,57 +1,73 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart){
-  cart = [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
+loadFromStorage();
 
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
   }
-  ];
 }
 
-
-
-function saveToStorage(){
+function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
-//can only save strings. so, convert cart into string
 
-
-export function addToCart(productId){
+export function addToCart(productId) {
   let matchingItem;
 
-  cart.forEach((cartItem) =>{
-    if(productId === cartItem.productId){
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
 
-  if(matchingItem){
+  if (matchingItem) {
     matchingItem.quantity += 1;
-  }else{
+  } else {
     cart.push({
-    productId: productId,
-    quantity: 1
-  });
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
   }
 
   saveToStorage();
 }
 
-export function removeFromCart(productId){
+export function removeFromCart(productId) {
   const newCart = [];
+
   cart.forEach((cartItem) => {
-    if(cartItem.productId !== productId){
+    if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
 
   cart = newCart;
-  saveToStorage();
 
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
 }
