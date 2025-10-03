9272ac738d18a325e33b8021e1033ddc2eff599f
Jake Huey <125854542+jhueyy@users.noreply.github.com>
2025-10-03 13:06:11 -0700

    converted code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,64 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+//start promise then call load products
+// once products are finished loading, runs anon function then display finished loading, then call resolve
+// which THEN displays next step
+
+// so here, we waited for load products to finish, then called resolve to go to next step
+
+Promise.all([
+  new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+}),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+*/
+
+/*
+// load products is async
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+ 
+});
+
+*/
+
+
